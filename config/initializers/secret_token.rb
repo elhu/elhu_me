6f4d5320519ccf2a19c18d594fe2022ba057cf0c
elhu <boisard.v@gmail.com>
2011-05-22 16:11:54 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElhuMe::Application.config.secret_token = 'e734e25996e1ef725bc10648086d17f31b7986847c7e35b9b1713f48950935ff28d121934ae154d89e6f303cc5bdc5f39807c127c45c1a56e910c902941939d5'

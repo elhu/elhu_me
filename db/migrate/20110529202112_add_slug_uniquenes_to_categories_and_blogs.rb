c33868427a8afd5bb5c68ef4d06a868a5628029e
elhu <boisard.v@gmail.com>
2011-05-29 22:40:59 +0100

    changed migration

--- db/migrate/20110529202112_add_slug_uniquenes_to_categories_and_blogs.rb
+++ db/migrate/20110529202112_add_slug_uniquenes_to_categories_and_blogs.rb
@@ -1,7 +1,6 @@
 class AddSlugUniquenesToCategoriesAndBlogs < ActiveRecord::Migration
   def self.up
     add_index :blogs, :slug, :unique => true
-    add_index :categories, :slug, :unique => true
   end
 
   def self.down

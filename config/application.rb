c1668d6139f0b2c43cdf509290fa71d49c8c5247
Vincent Boisard <elhu@vincent.chugulu.local>
2011-09-05 19:10:42 +0200

    tryimng to fix assets in prod

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,11 @@ require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(:default, :assets, Rails.env) if defined?(Bundler)
 
 module ElhuMe
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
